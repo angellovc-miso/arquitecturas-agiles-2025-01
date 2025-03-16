01415ba9c8bbfaa7f3d38723f219049a5ffd2860
angellovc-miso <a.villegasc2@uniandes.edu.co>
2025-03-16 16:00:24 -0500

    fixes order schema

--- sec_experiment/orden_de_pedido_service/modelos/OrdenPedidoSchema.py
+++ sec_experiment/orden_de_pedido_service/modelos/OrdenPedidoSchema.py
@@ -24,18 +24,30 @@ class Pedido(db.Model):
     cliente_id = db.Column(db.Integer, nullable=False)
     vendedor_id = db.Column(db.Integer, nullable=True)
 
+from marshmallow import fields, ValidationError
+
 class EnumMap(fields.Field):
+    def __init__(self, enum, **kwargs):
+        super().__init__(**kwargs)
+        self.enum = enum
+
     def _serialize(self, value, attr, obj, **kwargs):
         if value is None:
             return None
-        return {value.name: value.value}
+        return value.value  # Return only the value as a string
+
+    def _deserialize(self, value, attr, data, **kwargs):
+        try:
+            return self.enum(value)
+        except ValueError:
+            raise ValidationError(f"Invalid value '{value}' for enum {self.enum.__name__}.")
+
 
 class OrdenPedidoSchema(SQLAlchemyAutoSchema):
-    tipoDePago = EnumMap(attribute=('tipoDePago'))
-    estado = EnumMap(attribute='estado')
+    tipoDePago = EnumMap(enum=Pago, attribute='tipoDePago')
+    estado = EnumMap(enum=EstadoPedido, attribute='estado')
     class Meta:
         model = Pedido
         load_instance = True
-
 
 orden_pedido_schema = OrdenPedidoSchema(session=db.session)
